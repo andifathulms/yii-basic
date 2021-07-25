a8fef9c4c92f5f118399bb59be60772b89a06d59
Andi Fathul Mukminin <35810127+andifathulms@users.noreply.github.com>
2021-07-25 21:07:43 +0700

    Add files via upload

---
+++ controllers/MahasiswaController.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Mahasiswa;
+use app\models\MahasiswaSearch;
+use app\models\Prodi;
+use app\models\Provinces;
+use app\models\Regencies;
+use app\models\Districts;
+use app\models\Villages;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use app\models\UploadForm;
+
+/**
+ * MahasiswaController implements the CRUD actions for Mahasiswa model.
+ */
+class MahasiswaController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Mahasiswa models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new MahasiswaSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Mahasiswa model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Mahasiswa model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Mahasiswa();
+
+        if ($model->load(Yii::$app->request->post())) {
+            $model->tgl = \Yii::$app->formatter->asDate($model->tgl, "yyyy-MM-dd");
+
+            $model->save(false);
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Mahasiswa model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            $model->tgl = \Yii::$app->formatter->asDate($model->tgl, "yyyy-MM-dd");
+            $model->save();
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionUpload()
+    {
+        $model = new UploadForm();
+
+        if (Yii::$app->request->isPost) {
+            $model->foto = UploadedFile::getInstance($model, 'foto');
+            if ($model->upload()) {
+                // file is uploaded successfully
+                return;
+            }
+        }
+
+        return $this->render('upload', ['model' => $model]);
+    }
+
+    /**
+     * Deletes an existing Mahasiswa model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    public function actionSubcat(){
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $out = [];
+        if (isset($_POST['depdrop_parents'])) {
+            $parents = $_POST['depdrop_parents'];
+            if ($parents != null) {
+                $prodi = $parents[0];
+                $out = Prodi::getProdiList($prodi);
+
+                return ['output'=>$out,'selected'=>''];
+            }
+        }
+        return ['output'=>'','selected'=>''];
+    }
+    
+    public function actionKab(){
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $out = [];
+        if (isset($_POST['depdrop_parents'])) {
+            $parents = $_POST['depdrop_parents'];
+            if ($parents != null) {
+                $kab = $parents[0];
+                $out = Regencies::getKab($kab);
+
+                return ['output'=>$out,'selected'=>''];
+            }
+        }
+        return ['output'=>'','selected'=>''];
+    }
+
+    public function actionKel(){
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $out = [];
+        if (isset($_POST['depdrop_parents'])) {
+            $ids = $_POST['depdrop_parents'];
+            #$kab = empty($ids[0]) ? null : $ids[0];
+            $kab = $ids[0];
+            $kelurahan = empty($ids[1]) ? null : $ids[1];
+            if ($kab != null) {
+                $data = Districts::getKelurahan($kab);
+
+                // return ['output'=>$out,'selected'=>''];
+                return ['output'=>$data['out'], 'selected'=>$data['selected']];
+            }else{
+
+            }
+            
+        }
+        return ['output'=>'','selected'=>''];
+    }
+
+
+    /**
+     * Finds the Mahasiswa model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Mahasiswa the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Mahasiswa::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
